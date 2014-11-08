af9b5d23b5bcebb8605b94b2977b176309198c30
Jennifer Jiyue Wang <jiyue@stanford.edu>
2014-11-08 09:18:55 -0800

    BinaryTreeLevelOrderTraversal.py done, but use DFS instead of BFS

---
+++ BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,32 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+	# @param root, a tree node
+	# @return a list of lists of integers
+	"""
+	Careful: use DFS
+	"""
+	def preOrderTraverse(self, root, level ,res):
+		if root:
+			if len(res) < level + 1:
+				res.append([])
+			res[level].append(root.val)
+			self.preOrderTraverse(root.left, level + 1, res)
+			self.preOrderTraverse(root.right, level + 1, res)
+
+	def levelOrder(self, root):
+		res = []
+		level = 0
+		self.preOrderTraverse(root, level, res)
+		return res
+
+
+
+
+
+
